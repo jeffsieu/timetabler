8b4446cc365ef66a690007ce367a2576826132e0
LMAOboxhack <linfangyuan99@gmail.com>
2021-01-08 15:36:38 +0800

    form styling

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core'
 
-function SearchForm() {
+const useStyles = makeStyles((theme) => ({
+  form: {
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(2, 0, 2),
+  },
+}));
+
+const API_URL = 'https://api.nusmods.com/v2/2020-2021/modules'
+
+export default function SearchFrom() {
+  const classes = useStyles();
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <form>
+      <form className={classes.form}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
               variant="outlined"
-              required
               fullWidth
               id="mod"
               label="Module Code"
@@ -24,6 +37,7 @@ function SearchForm() {
           </Grid>
         </Grid>              
         <Button
+          className={classes.submit}
           type="submit"
           fullWidth
           variant="contained" 
@@ -32,8 +46,6 @@ function SearchForm() {
           Search
         </Button>
       </form>
-    </Container>
+    </Container>    
   );
 }
-
-export default SearchForm;
