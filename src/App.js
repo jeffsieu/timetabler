bae83100779f96684bc95764d4e97cfe6719ff19
Jeff Sieu <jeffsy00@gmail.com>
2021-01-08 15:45:32 +0800

    Add heading

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import axios from 'axios'
 import { createStore } from 'redux'
 import { createSlice } from '@reduxjs/toolkit'
-import { Box, Container, makeStyles } from '@material-ui/core'
+import { Box, Card, Container, makeStyles, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles({
   row: {
     // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     // border: 0,
     borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     // color: 'white',
     height: 48,
+    borderTop: '1px solid',
+    borderColor: 'black',
     // padding: '0 30px',
+  },
+  dayOfWeek: {
+    // boxShadow: '10px 0 5px -2px #888',
+    width: 100,
   },
   slot: 
   {
@@ -42,22 +47,12 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Box display="flex" className={classes.row}>
-          <Box width={100}>
-          </Box>
-          
-          {
-            slots.map(slot => 
-              <Box flex='1' className={classes.slot} borderColor="black">
-                {slot.start}
-              </Box>
-            )
-          }
-        </Box>
-        {weeks.map(week => 
+        <Typography variant="h4">
+          timetabler
+        </Typography>
+        <Card>
           <Box display="flex" className={classes.row}>
             <Box width={100}>
-              {week}
             </Box>
             
             {
@@ -68,7 +63,22 @@ function App() {
               )
             }
           </Box>
-        )}
+          {weeks.map(week => 
+            <Box display="flex" className={classes.row}>
+              <Box className={classes.dayOfWeek}>
+                {week}
+              </Box>
+              
+              {
+                slots.map(slot => 
+                  <Box flex='1' className={classes.slot} borderColor="black">
+                    {slot.start}
+                  </Box>
+                )
+              }
+            </Box>
+          )}
+        </Card>
       </Container>
     </div>
   );
