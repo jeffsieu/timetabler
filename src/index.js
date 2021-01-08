95d9f6ce1cdc6299d409c0cef4a34c9da92a230f
Jonathan <jon.lee.jing.yu@gmail.com>
2021-01-08 16:56:50 +0800

    Added delete modules. Added fetch all modules.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { fetchAllModules } from './redux/allModulesSlice'
 
+store.dispatch(fetchAllModules())
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -18,4 +20,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
